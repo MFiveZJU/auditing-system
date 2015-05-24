159b7cd1225722594627fb00a7c0601f92eb2ab5
stephenwww <wqstu1@gmail.com>
2015-05-24 13:31:38 +0800

    add signup template and css

--- auditing_system/settings.py
+++ auditing_system/settings.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = 'auditing_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        # 'TEMPLATE_DIRS': ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- auditor_management/views.py
+++ auditor_management/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    # return HttpResponse("Hello, world. You're at the auditor_management index.")
+    return render(request, 'auditor_management/signup.html')
+
+
+def signup(request):
+    return render(request, 'auditor_management/signup.html')
